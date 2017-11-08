8842009ef1d49ab3e3d9f82879f8f036e012e8e5
jereprz94 <jereprz1994@gmail.com>
2017-11-07 23:38:32 -0300

    modificacion del boton nocturno

--- TP5/EntregaFinal/js/script.js
+++ TP5/EntregaFinal/js/script.js
@@ -9,6 +9,9 @@ $("#nocturno").on("click", function(){
     $("#buscar").css("border-color", "#141d26");
     $("#buscar").css("color", "#FFF");
     mnoc = true;
+    $("#nocturno").css("color", "#3A9CF2")
+    $("#nocturno").css("font-weight", "bold")
+
   }
   else {
     $("body").css("background-color", "#e6ecf0");
@@ -17,11 +20,27 @@ $("#nocturno").on("click", function(){
     $("#buscar").css("border-color", "#ccc");
     $("#buscar").css("color", "#000");
     mnoc = false;
+    $("#nocturno").css("color", "#777")
+    $("#nocturno").css("font-weight", "normal")
   }
 
 })
 
 
-document.getElementsByClassName("imagen").addEventListener("oclick", function(e){
-	e.src.value = "";
-});
+$("#cont").append('<a href="#"><img class="imagen" src="images/paisaje.jpg" alt=" "></a>');
+$("#cont").append('<a href="#"><img class="imagen" src="images/images.jpg" alt=" "></a>');
+
+
+$("#imagen").on("click", function(){
+  $("#imagen").addClass("escalada");
+})
+
+// var pepe = document.getElementById('cont');
+
+// document.onload = function(){
+	// for(var n=1, n < 7, n++);
+	// pepe.innerHTML = '<a href="#"><img id="n" class="imagen" src="images\paisaje.jpg" alt=""></a>';
+
+
+
+// }
