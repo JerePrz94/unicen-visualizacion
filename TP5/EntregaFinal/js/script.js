10f7ee2a758af81f4bdba8cb79d7a7f53e4ac11d
jereprz94 <jereprz1994@gmail.com>
2017-11-16 19:49:27 -0300

    se agrego api y funcion carrousel

--- TP5/EntregaFinal/js/script.js
+++ TP5/EntregaFinal/js/script.js
@@ -1,9 +1,115 @@
 const vistaGrilla = 1;
 var mnoc = false;
 var modVista = vistaGrilla;
+var imagenes = [];
 
-// Modo Nocturno
-$(".nocturno").on("click", function(){
+// ---- API ----
+var cb = new Codebird;
+cb.setConsumerKey("4CJBqbebmf9IVwWBPG10qQD1q", "WL3dxEK9CEwqAeLzqfrI6L22rVCwBLKJahUnMocBVY2qStym25");
+cb.setToken("133798046-DvVwEcndjz3fKvEbnsIl2drgWxT2HX9mIhb2Kt11", "BOTqVdG18y5ZLjNc9DRI9dgmN1K7f6Ew2ZHC4r3a2zyCf");
+// cb.setProxy("https://cb-proxy.herokuapp.com/");
+
+var params = {
+    q: "#Tandil",
+    result_type: "mixed",
+    count: 100
+};
+cb.__call(
+    "search_tweets",
+    params,
+    function (reply) {
+      console.log(reply.httpstatus);
+      var tweets = reply.statuses;
+      console.log(tweets);
+      for (var i = 0; i < tweets.length; i++) {
+        if (tweets[i].extended_entities){
+          if (tweets[i].extended_entities.media.length > 1) {
+            for (var n = 0; n < tweets[i].extended_entities.media.length; n++) {
+              if (tweets[i].extended_entities.media[n].type == "photo") {
+                var imagen = {
+                  url: tweets[i].extended_entities.media[n].media_url,
+                  likes: tweets[i].favorite_count,
+                  user: tweets[i].user.name,
+                  text: tweets[i].text
+                };
+                imagenes.push(imagen);
+              }
+            }
+          }else if (tweets[i].extended_entities.media[0].type == "photo") {
+            var imagen = {
+              url: tweets[i].extended_entities.media[0].media_url,
+              likes: tweets[i].favorite_count,
+              user: tweets[i].user.name,
+              text: tweets[i].text
+            };
+            imagenes.push(imagen);
+          }
+        }
+      }
+      console.log(imagenes);
+      cargarImagenesGrilla();
+      cargarImagenes();
+    },
+    true // this parameter required
+);
+
+// ---- Carga las imagenes en la pag ----
+var lista = document.getElementById('lista');
+var carrousel = document.getElementById('carrousel');
+function cargarImagenes(){
+  for (var i = 0; i < imagenes.length; i++) {
+    var imgList = document.createElement('img');
+    imgList.src = imagenes[i].url;
+    imgList.id = i;
+    imgList.className = "imagen-f";
+    var a = document.createElement('a');
+    a.appendChild(imgList);
+    lista.appendChild(a);
+    var figure = document.createElement('figure');
+    figure.className = (i == 0) ? "figureCar select" : "figureCar posterior";
+    var divCar = document.createElement('div');
+    divCar.className = "divCar";
+    var imgCar = document.createElement('img');
+    imgCar.src = imagenes[i].url;
+    imgCar.className = "imgCar";
+    var pCar = document.createElement('p');
+    pCar.className = "likeCar";
+    pCar.innerHTML = imagenes[i].likes;
+    var spanCar = document.createElement('span');
+    spanCar.className = "glyphicon glyphicon-heart";
+    pCar.appendChild(spanCar);
+    divCar.appendChild(imgCar);
+    divCar.appendChild(pCar);
+    figure.appendChild(divCar);
+    carrousel.appendChild(figure);
+  }
+  console.log(carrousel);
+}
+
+
+var grilla =document.getElementById('cont');
+function cargarImagenesGrilla(){
+  for (var i = 0; i < imagenes.length; i++) {
+    var div = document.createElement('div');
+    div.className = "dimageng";
+    var a = document.createElement('a');
+    var img = document.createElement('img');
+    img.src = imagenes[i].url;
+    img.className = "imageng desvanecer";
+    var h3 = document.createElement('h3');
+    h3.innerHTML = imagenes[i].likes;
+    var span = document.createElement('span');
+    span.className = "glyphicon glyphicon-heart";
+    a.appendChild(img);
+    h3.appendChild(span);
+    div.appendChild(a);
+    div.appendChild(h3);
+    grilla.appendChild(div);
+  }
+}
+
+// ---- Modo Nocturno ----
+$("#nocturno").on("click", function(){
   if (mnoc == false) {
     $("body").css("background-color", "#141d26");
     $("body").css("color", "#FFF");
@@ -11,12 +117,9 @@ $(".nocturno").on("click", function(){
     $("#buscar").css("background-color", "#182430");
     $("#buscar").css("border-color", "#141d26");
     $("#buscar").css("color", "#FFF");
-    $("#buscarp").css("background-color", "#182430");
-    $("#buscarp").css("border-color", "#141d26");
-    $("#buscarp").css("color", "#FFF");
     mnoc = true;
-    $(".nocturno").css("color", "#3A9CF2");
-    $(".nocturno").css("font-weight", "bold");
+    $("#nocturno").css("color", "#3A9CF2");
+    $("#nocturno").css("font-weight", "bold");
     $(".like-f").css("background-color", "#243447");
   }
   else {
@@ -26,16 +129,14 @@ $(".nocturno").on("click", function(){
     $("#buscar").css("background-color", "#FFF");
     $("#buscar").css("border-color", "#ccc");
     $("#buscar").css("color", "#000");
-    $("#buscarp").css("background-color", "#FFF");
-    $("#buscarp").css("border-color", "#ccc");
-    $("#buscarp").css("color", "#000");
     mnoc = false;
-    $(".nocturno").css("color", "#777");
-    $(".nocturno").css("font-weight", "normal");
+    $("#nocturno").css("color", "#777");
+    $("#nocturno").css("font-weight", "normal");
     $(".like-f").css("background-color", "#FFF");
   }
 })
 
+// ---- Cambio de Vista ----
 $("#vista").on("click", function(){
   if (modVista == vistaGrilla){
     modVista = 2;
@@ -49,6 +150,32 @@ $("#vista").on("click", function(){
     $("#vist2").addClass("desactivar");
     $("#vista").addClass("glyphicon-blackboard");
     $("#vista").removeClass("glyphicon-th");
+  }
+});
+
+// ---- Carousel Movimiento ----
+var pos = 1;
+const flechaIzq = 37;
+const flechaDer = 39;
+document.addEventListener("keydown", function(e){
+  if (modVista != vistaGrilla) {
+    if (e.keyCode === flechaDer) {
+      if (carrousel.childElementCount > pos) {
+        carrousel.childNodes[pos].classList.remove('select');
+        carrousel.childNodes[pos].classList.add('anterior');
+        pos ++;
+        carrousel.childNodes[pos].classList.remove('posterior');
+        carrousel.childNodes[pos].classList.add('select');
+      }
+    }else if (e.keyCode === flechaIzq) {
+      if (pos > 1) {
+        carrousel.childNodes[pos].classList.remove('select');
+        carrousel.childNodes[pos].classList.add('posterior');
+        pos --;
+        carrousel.childNodes[pos].classList.remove('anterior');
+        carrousel.childNodes[pos].classList.add('select');
+      }
+    }
   }
 });
 
